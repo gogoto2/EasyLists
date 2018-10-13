843430259b778a0cc9cb8a662236394ed472549f
Stephen Gravrock <sdg@panix.com>
2018-10-13 16:57:18 -0700

    Initial Commit

---
+++ EasyLists/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyLists
+//
+//  Created by Steve Gravrock on 10/13/18.
+//  Copyright © 2018 Steve Gravrock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
