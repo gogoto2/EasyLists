e9dd693162500cf85e08e1f267a35fcdbaf7570e
Steve Gravrock <sdg@panix.com>
2018-10-14 12:10:20 -0700

    Merged overlapping UI tests
    Saves about 20 seconds test run time.

--- EasyListsUITests/EasyListsUITests.swift
+++ EasyListsUITests/EasyListsUITests.swift
@@ -21,64 +21,38 @@ class EasyListsUITests: XCTestCase {
         return app
     }
     
-    func testCreatesLists() {
-        let app = launchApp()
-        let table = app.tables["Lists"]
-        XCTAssertTrue(table.exists)
-        XCTAssertEqual(0, table.cells.count)
-        
-        app.addList(name: "List A")
-        app.addList(name: "List B")
-        
-        XCTAssertEqual(2, table.cells.count)
-        XCTAssertTrue(table.cells.element(boundBy: 0).descendants(matching: .staticText)["List A"].exists)
-        XCTAssertTrue(table.cells.element(boundBy: 1).descendants(matching: .staticText)["List B"].exists)
-    }
-    
-    func testCreatesItems() {
-        let app = launchApp()
-        app.addList(name: "List A")
-        app.tables["Lists"].cells.element(boundBy: 0).tap()
-        
-        let itemTable = app.tables["List A Items"]
-        XCTAssertTrue(itemTable.exists)
-        app.addItem(name: "Item 1")
-        app.addItem(name: "Item 2")
-        XCTAssertEqual(2, itemTable.cells.count)
-        XCTAssertTrue(itemTable.cells.element(boundBy: 0).descendants(matching: .staticText)["Item 1"].exists)
-        XCTAssertTrue(itemTable.cells.element(boundBy: 1).descendants(matching: .staticText)["Item 2"].exists)
-    }
-    
     func testPersistsListsAndItems() {
         var app = launchApp(resetData: true)
-        app.addList(name: "List C")
-        
+        app.addList(name: "List A")
+        app.addList(name: "List B")
+
         var listsTable = app.tables["Lists"]
         XCTAssertTrue(listsTable.exists)
-        XCTAssertEqual(1, listsTable.cells.count)
-        var listCell = listsTable.cells.element(boundBy: 0)
-        XCTAssertTrue(listCell.descendants(matching: .staticText)["List C"].exists)
-        listCell.tap()
+        XCTAssertEqual(2, listsTable.cells.count)
+        XCTAssertTrue(listsTable.cells.element(boundBy: 0).descendants(matching: .staticText)["List A"].exists)
+        XCTAssertTrue(listsTable.cells.element(boundBy: 1).descendants(matching: .staticText)["List B"].exists)
+        listsTable.cells.element(boundBy: 0).tap()
         
-        var itemsTable = app.tables["List C Items"]
+        var itemsTable = app.tables["List A Items"]
         XCTAssertTrue(itemsTable.exists)
         app.addItem(name: "Item 1")
-        var itemCell = itemsTable.cells.element(boundBy: 0)
-        XCTAssertTrue(itemCell.descendants(matching: .staticText)["Item 1"].exists)
-        
+        app.addItem(name: "Item 2")
+        XCTAssertTrue(itemsTable.cells.element(boundBy: 0).descendants(matching: .staticText)["Item 1"].exists)
+        XCTAssertTrue(itemsTable.cells.element(boundBy: 1).descendants(matching: .staticText)["Item 2"].exists)
+
         // Verify that the additions were persisted
         app = launchApp(resetData: false)
         listsTable = app.tables["Lists"]
         XCTAssertTrue(listsTable.exists)
-        XCTAssertEqual(1, listsTable.cells.count)
-        listCell = listsTable.cells.element(boundBy: 0)
-        XCTAssertTrue(listCell.descendants(matching: .staticText)["List C"].exists)
-        listCell.tap()
-        
-        itemsTable = app.tables["List C Items"]
+        XCTAssertEqual(2, listsTable.cells.count)
+        XCTAssertTrue(listsTable.cells.element(boundBy: 0).descendants(matching: .staticText)["List A"].exists)
+        XCTAssertTrue(listsTable.cells.element(boundBy: 1).descendants(matching: .staticText)["List B"].exists)
+        listsTable.cells.element(boundBy: 0).tap()
+
+        itemsTable = app.tables["List A Items"]
         XCTAssertTrue(itemsTable.exists)
-        itemCell = itemsTable.cells.element(boundBy: 0)
-        XCTAssertTrue(itemCell.descendants(matching: .staticText)["Item 1"].exists)
+        XCTAssertTrue(itemsTable.cells.element(boundBy: 0).descendants(matching: .staticText)["Item 1"].exists)
+        XCTAssertTrue(itemsTable.cells.element(boundBy: 1).descendants(matching: .staticText)["Item 2"].exists)
     }
 }
 
